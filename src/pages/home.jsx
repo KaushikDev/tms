7194446d81ab1b06415162c737b60e24ae111697
kaushikDev <piyush.kaushik02@gmail.com>
2025-01-21 04:54:37 +0530

    adding googleauth

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,26 @@
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { LABELS } from "../utilities/constants";
 import { ROUTES } from "../utilities/routes";
+import { useAuth } from "../context/authContext";
 
 const Home = () => {
+  const { googleLogin, loggedInUser}  = useAuth()
   const navigate = useNavigate();
 
-  const handleCTAClick = () => {
+  const handleRegularSignIn = () => {
     navigate(ROUTES.LOGIN);
   };
+
+  const handleGoogleSignIn = () => {
+    googleLogin();
+  };
+  
+  useEffect(()=>{
+    if(loggedInUser){
+      navigate(ROUTES.DASHBOARD)
+    }
+  }, [loggedInUser, navigate])
 
   return (
     <div className="container flex flex-col items-center justify-center text-center mx-auto p-4 bg-white-50 ">
@@ -16,12 +29,18 @@ const Home = () => {
       <div className="flex flex-row gap-4">
         <button
           className="w-full p-3 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 shadow-md hover:shadow-xl"
-          onClick={handleCTAClick}
+          onClick={handleRegularSignIn}
         >
           {LABELS.GET_STARTED}{" "}
         </button>
-        
+        <button
+          className="w-full p-3 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 shadow-md hover:shadow-xl"
+          onClick={handleGoogleSignIn}
+        >
+          {LABELS.GOOGLE_SIGNIN}{" "}
+        </button>
       </div>
+    
     </div>
   );
 };
